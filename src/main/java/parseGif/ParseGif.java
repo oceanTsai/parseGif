a5a94aa75a70cda769d4e2f5525a00782f37ec41
Ocean <ocean@caizhenhuadeMacBook-Pro.local>
2015-11-09 11:25:30 +0800

    使用maven建立基本結構。

---
+++ src/main/java/parseGif/ParseGif.java
@@ -0,0 +1,13 @@
+package parseGif;
+
+/**
+ * Hello world!
+ *
+ */
+public class ParseGif
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/parseGif/ParseGifTest.java
@@ -0,0 +1,38 @@
+package parseGif;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ParseGifTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ParseGifTest(String testName)
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ParseGifTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
